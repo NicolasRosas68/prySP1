010db5c5b9452dbbc05f91246fa34a4dbb7c180d
NicolasRosas68 <nicolasrosas68@gmail.com>
2023-05-11 23:20:35 -0300

    11/5/2023/23:20

---
+++ login.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace prySP1
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.lblModulo = new System.Windows.Forms.Label();
+            this.lblcontrasena = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtContrsena = new System.Windows.Forms.TextBox();
+            this.lstModulo = new System.Windows.Forms.ComboBox();
+            this.cmdAceptar = new System.Windows.Forms.Button();
+            this.cmdCancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsuario.Location = new System.Drawing.Point(39, 35);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(56, 19);
+            this.lblUsuario.TabIndex = 0;
+            this.lblUsuario.Text = "Usuario";
+            // 
+            // lblModulo
+            // 
+            this.lblModulo.AutoSize = true;
+            this.lblModulo.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblModulo.Location = new System.Drawing.Point(40, 148);
+            this.lblModulo.Name = "lblModulo";
+            this.lblModulo.Size = new System.Drawing.Size(57, 19);
+            this.lblModulo.TabIndex = 1;
+            this.lblModulo.Text = "Modulo";
+            // 
+            // lblcontrasena
+            // 
+            this.lblcontrasena.AutoSize = true;
+            this.lblcontrasena.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblcontrasena.Location = new System.Drawing.Point(39, 91);
+            this.lblcontrasena.Name = "lblcontrasena";
+            this.lblcontrasena.Size = new System.Drawing.Size(78, 19);
+            this.lblcontrasena.TabIndex = 2;
+            this.lblcontrasena.Text = "Contrasena";
+            this.lblcontrasena.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(163, 36);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(100, 20);
+            this.txtUsuario.TabIndex = 3;
+            // 
+            // txtContrsena
+            // 
+            this.txtContrsena.Location = new System.Drawing.Point(163, 91);
+            this.txtContrsena.Name = "txtContrsena";
+            this.txtContrsena.Size = new System.Drawing.Size(100, 20);
+            this.txtContrsena.TabIndex = 4;
+            // 
+            // lstModulo
+            // 
+            this.lstModulo.FormattingEnabled = true;
+            this.lstModulo.Location = new System.Drawing.Point(163, 145);
+            this.lstModulo.Name = "lstModulo";
+            this.lstModulo.Size = new System.Drawing.Size(100, 21);
+            this.lstModulo.TabIndex = 5;
+            // 
+            // cmdAceptar
+            // 
+            this.cmdAceptar.Location = new System.Drawing.Point(325, 36);
+            this.cmdAceptar.Name = "cmdAceptar";
+            this.cmdAceptar.Size = new System.Drawing.Size(75, 23);
+            this.cmdAceptar.TabIndex = 6;
+            this.cmdAceptar.Text = "Aceptar";
+            this.cmdAceptar.UseVisualStyleBackColor = true;
+            this.cmdAceptar.Click += new System.EventHandler(this.cmdAceptar_Click);
+            // 
+            // cmdCancelar
+            // 
+            this.cmdCancelar.Location = new System.Drawing.Point(325, 91);
+            this.cmdCancelar.Name = "cmdCancelar";
+            this.cmdCancelar.Size = new System.Drawing.Size(75, 23);
+            this.cmdCancelar.TabIndex = 7;
+            this.cmdCancelar.Text = "Cancelar";
+            this.cmdCancelar.UseVisualStyleBackColor = true;
+            this.cmdCancelar.Click += new System.EventHandler(this.cmdCancelar_Click);
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(445, 214);
+            this.Controls.Add(this.cmdCancelar);
+            this.Controls.Add(this.cmdAceptar);
+            this.Controls.Add(this.lstModulo);
+            this.Controls.Add(this.txtContrsena);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.lblcontrasena);
+            this.Controls.Add(this.lblModulo);
+            this.Controls.Add(this.lblUsuario);
+            this.Name = "frmLogin";
+            this.Text = "login";
+            this.Load += new System.EventHandler(this.frmLogin_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.Label lblModulo;
+        private System.Windows.Forms.Label lblcontrasena;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtContrsena;
+        private System.Windows.Forms.ComboBox lstModulo;
+        private System.Windows.Forms.Button cmdAceptar;
+        private System.Windows.Forms.Button cmdCancelar;
+    }
+}
+
---
+++ login.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prySP1
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmdCancelar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmdAceptar_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
